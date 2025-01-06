2712931cc4352029084b6b0c5154dfc90abb8e48
HAJITH-M <hajithm2003@gmail.com>
2025-01-06 16:29:05 +0530

    error resolved in the viteconfig.js

--- UTS-app/vite.config.js
+++ UTS-app/vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { config } from 'dotenv';
-config();
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': process.env
-  },
-  esbuild: {
-    loader: 'jsx', // Ensures .js files can have JSX
-    include: /src\/.*\.js$/, // Include only .js files in the src folder
-  },
 })
